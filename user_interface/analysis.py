3c2fa2cfd023d2f71ec77bc0d56ca38686289cc0
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-14 10:17:47 +0200

    Create analysis.py

---
+++ user_interface/analysis.py
@@ -0,0 +1,12 @@
+# Third-Party Imports
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def get_analysis():
+    return html.Div(
+        id="",
+        className="",
+        children=[
+            html.H2("The analysis section..."),
+        ]
+    )
