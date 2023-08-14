d2e817c62a4860ca6699cc2e1dee28ef35ecd84f
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-14 10:17:54 +0200

    Create newsbot.py

---
+++ user_interface/newsbot.py
@@ -0,0 +1,12 @@
+# Third-Party Imports
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def get_newsbot():
+    return html.Div(
+        id="",
+        className="",
+        children=[
+            html.H2("The newsbot section..."),
+        ]
+    )
