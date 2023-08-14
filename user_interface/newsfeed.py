c1c69eb182b8bac80fed89e765031bd697b4cf0a
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-14 10:17:56 +0200

    Create newsfeed.py

---
+++ user_interface/newsfeed.py
@@ -0,0 +1,12 @@
+# Third-Party Imports
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def get_newsfeed():
+    return html.Div(
+        id="",
+        className="",
+        children=[
+            html.H2("The newsfeed section..."),
+        ]
+    )
