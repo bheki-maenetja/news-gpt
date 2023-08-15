0ce99cc42ece3d87bdb76e59c9acfe2d5bf476e3
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-15 19:49:54 +0200

    completed basic layout of main article feed

--- user_interface/newsfeed.py
+++ user_interface/newsfeed.py
@@ -2,23 +2,23 @@
 from dash import html, dcc
 import dash_bootstrap_components as dbc
 
-def get_news_card():
-    return dbc.Card(
+def get_news_card(headline, img_url):
+    formatted_hl = headline if len(headline) < 101 else headline[:101] + "..."
+
+    return html.Div(
         id="",
-        class_name="news-card",
+        className="news-card",
         children=[
-            dbc.CardImg(src="/assets/placeholder.jpg", top=True),
-            dbc.CardBody(
-                children=[
-                    html.P("News article headline"),
-                    html.Hr(),
-                    html.P("News article description"),
-                ]
-            ),
-        ]
+            html.P(
+                id="",
+                className="news-card-headline",
+                children=formatted_hl,
+            )
+        ],
+        style={"backgroundImage": f"url({img_url})"}
     )
 
-def get_newsfeed():
+def get_newsfeed(headlines):
     return html.Div(
         id="newsfeed-section",
         className="newsfeed-section",
@@ -27,8 +27,49 @@ def get_newsfeed():
                 id="main-article-feed",
                 className="main-article-feed",
                 children=[
-                    get_news_card()
-                    for _ in range(100)
+                    html.Div(
+                        id="newsfeed-params",
+                        className="newsfeed-params",
+                        children=[
+                            html.H2("Top Headlines"),
+                            dbc.Select(
+                                id="category-select",
+                                class_name="category-select",
+                                placeholder="Select a News Category",
+                                value=None,
+                                persistence=False,
+                                options=[
+                                    {"label": "General", "value": "general"},
+                                    {"label": "Business", "value": "business"},
+                                    {"label": "Technology", "value": "technology"},
+                                    {"label": "Science", "value": "science"},
+                                    {"label": "Health", "value": "health"},
+                                    {"label": "Sports", "value": "sports"},
+                                    {"label": "Entertainment", "value": "entertainment"},
+                                ]
+                            ),
+                            dbc.Select(
+                                id="country-select",
+                                class_name="country-select",
+                                placeholder="Select a Country",
+                                value=None,
+                                persistence=False,
+                                options=[
+                                    {"label": "United States", "value": "us"},
+                                    {"label": "United Kingdom", "value": "gb"},
+                                    {"label": "South Africa", "value": "za"},
+                                ]
+                            ),
+                        ]
+                    ),
+                    html.Div(
+                        id="newsfeed-articles",
+                        className="newsfeed-articles",
+                        children=[
+                            get_news_card(h["title"], h["urlToImage"])
+                            for h in headlines
+                        ]
+                    )
                 ]
             ),
             html.Div(
