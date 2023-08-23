aaadc5c777d260485b3735eaa37f3383152d5431
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-23 12:24:51 +0200

    Update articles.py

--- articles/articles.py
+++ articles/articles.py
@@ -9,7 +9,6 @@ def get_top_headlines():
     try:
         client = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
         headlines = client.get_top_headlines()
-        print(headlines)
     except Exception as e:
         print(f"Something is wrong: {e}")
         return {}
