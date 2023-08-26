6a14720d5370ae17b35e7fe11e9e0ea0fbc9f120
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-26 10:49:48 +0200

    restricted headlines to certain categories and countries

--- articles/articles.py
+++ articles/articles.py
@@ -8,7 +8,7 @@ import os
 def get_top_headlines():
     try:
         client = NewsApiClient(api_key=os.getenv("NEWS_API_KEY"))
-        headlines = client.get_top_headlines()
+        headlines = client.get_top_headlines(category="general", country="us")
     except Exception as e:
         print(f"Something is wrong: {e}")
         return {}
