4791b219aa92a16594cc7694868f63a3567f2a4c
bheki-maenetja <bhekimaenetja@gmail.com>
2023-08-30 08:39:42 +0200

    created function to build prompts for headline bot

--- nlp/summariser.py
+++ nlp/summariser.py
@@ -9,7 +9,13 @@ def build_headline_list(headlines):
     return "+".join(h["title"] for _, h in headlines.iterrows())
 
 # Building prompts
-def build_prompt(headline_list, sum_format):
+def build_headline_bot_prompt(headline_list, user_prompt):
+    main_prompt = "You will be given a user-generated prompt and a list of news headline. Each headline is separated by a '+' symbol. Answer the prompt using the list of headlines. YOU MAY ONLY USE THE HEADLINES FOR CONTEXT!\n\n"
+    main_prompt += f"Prompt: {user_prompt}\n\n"
+    main_prompt += f"Headlines: {headline_list}"
+    return main_prompt
+
+def build_summary_prompt(headline_list, sum_format):
     main_prompt = "Summarise the following list of news headlines. Each headline is separated by a '+' symbol. The summary should be in the form of "
     
     format_prompt = {
@@ -30,14 +36,8 @@ def build_prompt(headline_list, sum_format):
     main_prompt += f" Headlines: {headline_list}"
     return main_prompt
 
-# Summarisation
-def summarise_headlines(headlines, sum_format):
-    headline_list = build_headline_list(headlines)
-    prompt = build_prompt(headline_list, sum_format)
-    summary = gpt_summary(prompt)
-    return summary
-
-def gpt_summary(prompt):
+# GPT-4 API Call
+def gpt_call(prompt):
     openai.api_key = os.getenv("OPENAI_API_KEY")
     messages = [
         {"role": "user", "content": prompt},
@@ -54,4 +54,18 @@ def gpt_summary(prompt):
         summary = res.choices[0].message.content
         return summary
     except Exception as e:
-        return f"gpt_summary -> {e}"
+        return f"gpt_call -> {e}"
+
+# Summarisation
+def summarise_headlines(headlines, sum_format):
+    headline_list = build_headline_list(headlines)
+    prompt = build_summary_prompt(headline_list, sum_format)
+    summary = gpt_call(prompt)
+    return summary
+
+# Chatbots
+def headline_chatbot(headlines, prompt):
+    headline_list = build_headline_list(headlines)
+    prompt = build_headline_bot_prompt(headline_list, prompt)
+    message = gpt_call(prompt)
+    return message
