53ac60add0d1098f1552ba9c4bc69d0ffa04de13
bheki-maenetja <bhekimaenetja@gmail.com>
2023-09-01 09:13:09 +0200

    boilerplate for word cloud section

---
+++ user_interface/word_cloud.py
@@ -0,0 +1,12 @@
+# Third-Party Imports
+from dash import html, dcc
+import dash_bootstrap_components as dbc
+
+def get_word_cloud():
+    return html.Div(
+        id="",
+        className="",
+        children=[
+            html.H2("The word cloud section..."),
+        ]
+    )
