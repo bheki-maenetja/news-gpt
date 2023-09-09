548a2328f47b93b01dd6d410b765f2d18825ae53
bheki-maenetja <bhekimaenetja@gmail.com>
2023-09-09 21:01:03 +0200

    refactored code for delivering for main_nav to app.py

--- user_interface/main_nav.py
+++ user_interface/main_nav.py
@@ -1,45 +1,44 @@
 # Third-Party Imports
 from dash import html, dcc
 
-main_tabs = dcc.Tabs(
-    id="main-tabs",
-    className="main-tabs",
-    value="analysis",
-    vertical=False,
-    mobile_breakpoint=0,
-    children=[
-        dcc.Tab(
-            className="main-tab",
-            selected_className="main-tab-selected",
-            label="Analysis",
-            value="analysis",
-        ),
-        dcc.Tab(
-            className="main-tab",
-            selected_className="main-tab-selected",
-            label="Keywords",
-            value="word-cloud",
-        ),
-        dcc.Tab(
-            className="main-tab",
-            selected_className="main-tab-selected",
-            label="Editorial",
-            value="editorial",
-        ),
-        dcc.Tab(
-            className="main-tab",
-            selected_className="main-tab-selected",
-            label="NewsBot",
-            value="newsbot",
-        ),
-    ]
-)
-
-main_nav = html.Div(
-    id="main-nav",
-    className="main-nav",
-    children=[
-        html.H1("NewsGPT", className="nav-header"),
-        main_tabs,
-    ]
-)
+def get_main_nav():
+    return html.Div(
+        id="main-nav",
+        className="main-nav",
+        children=[
+            html.H1("NewsGPT", className="nav-header"),
+            dcc.Tabs(
+                id="main-tabs",
+                className="main-tabs",
+                value="analysis",
+                vertical=False,
+                mobile_breakpoint=0,
+                children=[
+                    dcc.Tab(
+                        className="main-tab",
+                        selected_className="main-tab-selected",
+                        label="Analysis",
+                        value="analysis",
+                    ),
+                    dcc.Tab(
+                        className="main-tab",
+                        selected_className="main-tab-selected",
+                        label="Keywords",
+                        value="word-cloud",
+                    ),
+                    dcc.Tab(
+                        className="main-tab",
+                        selected_className="main-tab-selected",
+                        label="Editorial",
+                        value="editorial",
+                    ),
+                    dcc.Tab(
+                        className="main-tab",
+                        selected_className="main-tab-selected",
+                        label="NewsBot",
+                        value="newsbot",
+                    ),
+                ]
+            )
+        ]
+    )
